7aa12612602f563926029e0b46b0d65de1c1bc2b
ShaiWebsite1 <suresh.k@shai.health>
2025-10-30 16:48:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false
+  devIndicators: false,
+
+  async redirects() {
+    return [
+      {
+        source: '/index.php',
+        destination: 'https://shai.health/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
